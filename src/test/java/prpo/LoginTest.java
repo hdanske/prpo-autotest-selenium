3bf134292466c1a1512cdc4ae86581fad00180b2
yprokopenko <yprokopenko@intervale.ru>
2020-04-06 09:20:52 +0300

    дописал тест

--- src/test/java/prpo/LoginTest.java
+++ src/test/java/prpo/LoginTest.java
@@ -1,14 +1,19 @@
 package prpo;
+
 import files.LoginPage;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static files.LoginPage.*;
@@ -32,16 +37,32 @@ public class LoginTest {
     public void unSuccessLogin() {
 
         loginPage.goTo(LOGIN_URL);
-        loginPage.typeUsername("admin");
-        loginPage.typePassword("ad");
+        loginPage.typeUsername("a");
+        loginPage.typePassword("a");
         loginPage.clickLogin();
         loginPage.checkMessage("Login failed");
         loginPage.checkMessage("Неправильные логин или пароль");
 
-        loginPage.clickCloseErrorWindow();
-        loginPage.clearField(USERNAME);
-        loginPage.checkMessage("Данное поле обязательно");
-        loginPage.clearField(PASSWORD);
+        WebElement xButton = driver.findElement(By.xpath("//p-messages//a"));
+        xButton.click();
+
+        driver.findElement(By.id("username")).sendKeys(Keys.BACK_SPACE);
+        driver.findElement(By.id("password")).sendKeys(Keys.BACK_SPACE);
+
+        List<WebElement> messages = driver.findElements(By.xpath("//span[text()='Данное поле обязательно']"));
+        Assert.assertTrue(messages.size() == 2);
+
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+        Assert.assertFalse(isClickable(driver, loginButton));
+
+
+
+
+
+//        loginPage.clickCloseErrorWindow();
+//        loginPage.clearField(USERNAME);
+//        loginPage.checkMessage("Данное поле обязательно");
+//        loginPage.clearField(PASSWORD);
 
 
 //        wait.until(urlToBe("https://prpo-test.intervale.ru/console/import-charges-trx"));
@@ -49,6 +70,19 @@ public class LoginTest {
 //        Assert.assertEquals(URL,"https://prpo-test.intervale.ru/console/import-charges-trx");
     }
 
+    public static boolean isClickable(WebDriver driver, WebElement element)
+    {
+        try
+        {
+            WebDriverWait wait = new WebDriverWait(driver, 5);
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
 
 
 //    @After
